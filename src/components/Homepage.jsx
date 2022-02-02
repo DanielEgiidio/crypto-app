d60e0f4b86916ddb26f6cdec9a3b765eaba24aaf
DanielEgidiio <danielegidioti@gmail.com>
2022-02-02 09:10:33 -0300

    Estruturação da Homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import millify from "millify";
+import { Typography, Row, Col, Statistic } from "antd";
+
+const { Title } = Typography;
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <>
+      <Title level={2} className="heading">
+        Estatisticas Globais de CryptoMoedas
+        <Row>
+          <Col span={12}>
+            <Statistic title="Total Cryptocurrencies" value="5" />
+          </Col>
+          <Col span={12}>
+            <Statistic title="Exchanges" value="5" />
+          </Col>
+          <Col span={12}>
+            <Statistic title="Capitalização de Mercado Total" value="5" />
+          </Col>
+          <Col span={12}>
+            <Statistic title="Total 24h Volume" value="5" />
+          </Col>
+          <Col span={12}>
+            <Statistic title="Mercados Totais " value="5" />
+          </Col>
+        </Row>
+      </Title>
+    </>
+  );
 };
 
 export default Homepage;
