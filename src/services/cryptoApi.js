34874474236742715e083dea199de4f313e792a8
DanielEgidiio <danielegidioti@gmail.com>
2022-02-05 18:14:53 -0300

    Implementação da Api e Redux Toolkit

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '29154913acmsh27e08f9f621e94fp11ef4ejsn4c2b1562a99c'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
